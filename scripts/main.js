a5c5d5a346a59c80d3e6f6e46b810ae8d98a2ac7
leahcarlin <leahrcarlin@gmail.com>
2021-09-15 13:33:06 +0200

    Counter when new item on list

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,52 @@
 const todoEl = document.querySelector("#todo");
 const todoListEl = document.querySelector(".todo-list")
 const submitBtn = document.querySelector("#submit");
-const sumTodoEl = document.querySelector("sum-todo");
-const sumDoneEl = document.querySelector("sum-done");
-const sumTotalEl = document.querySelector("sum-total");
+const sumTodoEl = document.querySelector("#sum-todo");
+const sumDoneEl = document.querySelector("#sum-done");
+const sumTotalEl = document.querySelector("#sum-total");
+let sumTodo = 0;
+let sumDone = 0;
+let sumTotal = 0;
 
-//functions
 
-function addToList(){
+function updateCounter(){
+  //update counter
+  sumTodo += 1;
+  sumTotal += 1;
+  sumTodoEl.innerText = `To Do: ${sumTodo}`;
+  sumTotalEl.innerText = `Total: ${sumTotal}`;
+}
+
+submitBtn.addEventListener("click", function(){
   const todoItem = todoEl.value;
-  const newInput = document.createElement("input");
-  newInput.type = "checkbox";
-  newInput.name = "todo-item";
-  newInput.id = "todo-item";
-  
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = "todo-item";
+  checkbox.id = "todo-item";
+
   // create label for checkbox
-  const label = document.createElement("label");
-  label.htmlFor = "todo-item";
-  label.appendChild(document.createTextNode(todoItem));
+  const checkboxLabel = document.createElement("label");
+  checkboxLabel.className = "todo-label";
+  checkboxLabel.htmlFor = "todo-item";
+  checkboxLabel.appendChild(document.createTextNode(todoItem));
   const br = document.createElement("br");
 
-  todoListEl.appendChild(newInput);
-  todoListEl.appendChild(label);
+  todoListEl.appendChild(checkbox);
+  todoListEl.appendChild(checkboxLabel);
   todoListEl.appendChild(br);
   todoEl.value = '';
 
-
-}
-
-
-submitBtn.addEventListener("click", function(){
-  addToList();
+  updateCounter();
 })
+
+
+/*update counter when box is checked
+checkbox.addEventListener("change", function() {
+  if (this.checked) {
+    todoListEl.classList.remove("todo-list")
+    todoListEl.classList.add("todo-list-completed");
+  } else {
+    todoListEl.classList.remove("todo-list-completed")
+    todoListEl.classList.add("todo-list");        
+  }
+}); */
