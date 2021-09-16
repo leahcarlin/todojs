31ceafedf7d0fc4687e1850920304e160b5d7127
leahcarlin <leahrcarlin@gmail.com>
2021-09-16 14:00:21 +0200

    CSS for checked box

--- scripts/main.js
+++ scripts/main.js
@@ -9,44 +9,44 @@ let sumDone = 0;
 let sumTotal = 0;
 
 
-function updateCounter(){
-  //update counter
+// Update counter when new todo is added
+function newTodoCounter(){
   sumTodo += 1;
   sumTotal += 1;
   sumTodoEl.innerText = `To Do: ${sumTodo}`;
   sumTotalEl.innerText = `Total: ${sumTotal}`;
 }
 
+
+//validation for value field
+function addTodo(){
+  let todoItem = todoEl.value;
+  if (todoItem.length == 0){
+    alert("Please enter a todo item");
+  } else {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox-css"
+    checkbox.name = "checkbox";
+    checkbox.id = "checkbox";
+
+    // create label for checkbox
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.className = "checkbox-label";
+    checkboxLabel.htmlFor = "checkbox";
+    checkboxLabel.appendChild(document.createTextNode(todoItem));
+    const br = document.createElement("br");
+
+    todoListEl.appendChild(checkbox);
+    todoListEl.appendChild(checkboxLabel);
+    todoListEl.appendChild(br);
+    newTodoCounter();
+    todoEl.value = '';
+  }
+}
+
+//add todo to list after submit
 submitBtn.addEventListener("click", function(){
-  const todoItem = todoEl.value;
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.name = "todo-item";
-  checkbox.id = "todo-item";
-
-  // create label for checkbox
-  const checkboxLabel = document.createElement("label");
-  checkboxLabel.className = "todo-label";
-  checkboxLabel.htmlFor = "todo-item";
-  checkboxLabel.appendChild(document.createTextNode(todoItem));
-  const br = document.createElement("br");
-
-  todoListEl.appendChild(checkbox);
-  todoListEl.appendChild(checkboxLabel);
-  todoListEl.appendChild(br);
-  todoEl.value = '';
-
-  updateCounter();
+  addTodo();
 })
 
-
-/*update counter when box is checked
-checkbox.addEventListener("change", function() {
-  if (this.checked) {
-    todoListEl.classList.remove("todo-list")
-    todoListEl.classList.add("todo-list-completed");
-  } else {
-    todoListEl.classList.remove("todo-list-completed")
-    todoListEl.classList.add("todo-list");        
-  }
-}); */
